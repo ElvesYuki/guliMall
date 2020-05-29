ccf336ef9a26e22265bb2679b2db97bbf3ba497c
LUOHUAN <1026770043@qq.com>
2020-05-29 09:25:40 +0800

    初始化项目

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/profuct/GuliMailProfuctApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.profuct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ThinkPad
+ */
+@SpringBootApplication
+public class GuliMailProfuctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliMailProfuctApplication.class, args);
+    }
+
+}
